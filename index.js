24c37b9703ea6cd82241acf44315518723f3cfb2
orozmat <dj.orozmat.kd@gmail.com>
2024-03-14 15:15:12 +0300

    lesson_12

---
+++ index.js
@@ -0,0 +1,17 @@
+// $(document).ready(function(){
+   
+//     $('#hello').text('Hello, world!');
+    
+   
+//     var newParagraph = $('<p>').text('Этот текст генерируется динамически');
+//     $('body').append(newParagraph);
+    
+   
+//     $('li').addClass('list-item');
+    
+   
+//     $('a').attr('target', '_blank');
+    
+   
+//     $('#hello').remove();
+// });
